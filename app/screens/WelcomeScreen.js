c6f439f814596fa059833f30788ec8ff2cfca3de
CollinsTatang <makungongcollins@gmail.com>
2022-11-19 16:19:30 +0100

    Add welcome page

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,137 +1,133 @@
 import React from 'react';
-import { Button, Image, ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-function WelcomeScreen(props) {
+const WelcomeScreen = (props) => {
     return (
-        <View style={styles.container}>
+       <ScrollView style={{width:"100%"}} >
 
-            <Image
-                style={styles.logo} 
-                source={require('../assets/images/logo.png')} />
-            <View style={styles.contactButton}>
-                <Button title = "Contact us" color="#111" />
-            </View>       
+            <View style={styles.container}> 
+                    <Navbar />
 
-            <View style={styles.textContainer}>
+                    <View style={styles.mainContainer}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.comingSoon}>Coming Soon</Text>
+                            <Text style={styles.textHead}>Bringing Muslims Together</Text>                          
+                        </View>
+                        <View style={styles.playIcon} >
+                                <Image style={styles.playIcon1} source={require('../assets/images/google-play.png')} />
+                                <Image style={styles.playIcon2} source={require('../assets/images/apple-store.png')} />
+                        </View>
+                            <Image 
+                            resizeMode="contain"
+                            style={styles.landingImage}
+                            source={require('../assets/images/landing-image-2.png')} />
+                        
+                            
+                        
+                         <View style={styles.textContainer2}>
 
-                <Text style={styles.comingSoon}>Coming Soon</Text>
-                
-                <Text style={styles.textHead}>Bringing Muslims Together</Text>
+                            <Text style={styles.comingSoon2}>Coming Soon</Text>
+                            <Text style={styles.textHead}>Get Notified When we Launch</Text>
+                            <TextInput style={{height: 40, width: "100%"}} placeholder=""></TextInput>
+                            <Text style={styles.textHead}>Your email address has been received.</Text>
+                            <Text style={styles.textHead}>Don't worry, we won't spam you :)</Text>
+                        </View>
+                        <Image 
+                            resizeMode="contain"
+                            style={styles.landingImage}
+                            source={require('../assets/images/landing-image-1.png')} />
+                                              
+                       <View style={styles.social}>
+                            <Image style={styles.socialIcon} source={require('../assets/images/facebook.png')} />
+                            <Image style={styles.socialIcon} source={require('../assets/images/twitter.png')} />
+                            <Image style={styles.socialIcon} source={require('../assets/images/tiktok.png')} />
+                            <Image style={styles.socialIcon} source={require('../assets/images/youtube.png')} />
+                        </View>
+                    </View>
 
-                <Image style={styles.playIcon1} source={require('../assets/images/google-play.png')} />
+                    <Footer />
 
-                <Image style={styles.playIcon2} source={require('../assets/images/apple-store.png')} />
-            </View>
-            
-                 <ImageBackground 
-                resizeMode="contain"
-                style={styles.landingImage}
-                source={require('../assets/images/landing-image-2.png')} />
-            
-            <View style={styles.textContainer2}>
-                <Text style={styles.comingSoon2}>Coming Soon</Text>
-                <Text style={styles.textHead}>Get Notified When we Launch</Text>
-                <TextInput style={{height: 40, width: "100%"}} placeholder=""> </TextInput>
-                <Text style={styles.textHead}>Your email address has been received.</Text>
-                <Text style={styles.textHead}>Don't worry, we won't spam you :)</Text>
-            </View>
-             
-               
-            <View style={styles.social}>
-                <Image style={styles.socialIcon} source={require('../assets/images/facebook.png')} />
-                <Image style={styles.socialIcon} source={require('../assets/images/twitter.png')} />
-                <Image style={styles.socialIcon} source={require('../assets/images/tiktok.png')} />
-                <Image style={styles.socialIcon} source={require('../assets/images/youtube.png')} />
-            </View>
-            
-            <Text style={styles.footer}>Copyright © 2022 Soora. All rights reserved</Text>
-        </View>
-    
-        
-        
-    );
-}
+                </View>
+        </ScrollView>
+)}
 
 const styles = StyleSheet.create({
+    mainContainer: {
+    justifyContent: "center",
+    },
+    comingSoon: {
+        marginTop: 2,
+    },
+    comingSoon2: {
+
+    },
+    textHead: {
+        fontFamily: "Inter",
+        fontStyle: "normal",
+        fontSize: 36,
+        marginBottom: 5,
+
+    },
     container: {
         backgroundColor: "#fff",
-        justifyContent: "flex-end",
-        alignItems: "center",
-        flex: 1,   
+        marginTop: 5,backgroundColor: "#fff",
+        flexDirection: "row",
+        justifyContent: "space-between", 
+        paddingTop: 25,
+        flex: 1,
+        marginLeft: 3,
+        marginRight: 3,
+        flex: 1,
     },
-    logo: {
-        position: "absolute",
-        top: 40,
-        left: 30,
-        alignItems: "flex-start"
-    },
-    contactButton: {
-        position: "absolute",
-        alignItems: "flex-end",
-        top: 40,
-        right: 30,   
+    landingImage: {
+        height: 890,
+        width:"100%", 
     },
     textContainer: {
         backgroundColor: "#fff",
-        position: "absolute",
-        flexDirectiona: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "30%",
-        top: 70,
-    
+        marginTop: 5,
+        marginLeft: 3,
+        marginRight: 3,
     },
-    landingImage: {
-        position: "relative",
-        top: 300,
-        width: "100%",
-        height: "100%",
-        
+        container: {
+        backgroundColor: "#fff",
+        marginTop: 5,
+        marginLeft: 3,
+        marginRight: 3,
+        flex: 1,
     },
 
     textContainer2: {
         backgroundColor: "#fff",
-        position: "relative",
-        flexDirectiona: "row",
         justifyContent: "center",
         alignItems: "center",
-        width: "100%",
-        height: "30%",
-        top: 70
-        
+        marginBottom: 5, 
+    },
+    playIcon: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+
     },
     playIcon1: { 
-        left: 30,
-        justifyContent: "flex-end",
-        width: "50%",
-        alignItems: "flex-end",
+        marginRight: 5,
     },
     playIcon2: {
-        right: 30,
-        width: "50%",
-        alignItems: "flex-start",
-        justifyContent: "flex-start",
-       
+      
+        
     },
-    
-    textContainer3: {
-        backgroundColor: "#fff",
-        position: "relative",
-        width: 24,
-        height: 24,
-        top: 8,
+    social:{
+        flexDirection: "row",
+        justifyContent: "space-evenly",
     },
-    social: {
-        position: "relative",
-        width: 24,
-        height: 24,
-        left: 8,
-        top: 8,
-    },
-    footer: {
-
+    socialIcon: {
+        padding: 2,
+        borderStyle: "solid",
+        borderColor: "#000",
+        borderRadius: 100, 
     },
     
 });
+
 export default WelcomeScreen;
